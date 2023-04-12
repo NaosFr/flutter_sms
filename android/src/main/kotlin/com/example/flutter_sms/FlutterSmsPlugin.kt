c247ebf4992742fa3d4888d752cb98b70198c8a4
Nicolas CELLA <nicolas.cella.nc@gmail.com>
2023-04-12 18:11:38 +0200

    fix 7

--- android/src/main/kotlin/com/example/flutter_sms/FlutterSmsPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_sms/FlutterSmsPlugin.kt
@@ -1,41 +1,100 @@
 package com.example.flutter_sms
 
 import android.annotation.TargetApi
+import android.app.Activity
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.telephony.SmsManager
+import android.util.Log
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
-import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
-import android.app.Activity
-import android.net.Uri
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 
-class FlutterSmsPlugin(registrar: Registrar) : MethodCallHandler {
+import androidx.activity.result.contract.ActivityResultContracts
+
+
+class FlutterSmsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+  private lateinit var mChannel: MethodChannel
+  private var activity: Activity? = null
   private val REQUEST_CODE_SEND_SMS = 205
 
-  var activity: Activity? = null
-  private var result: Result? = null
+	var result: Result? = null
 
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    setupCallbackChannels(flutterPluginBinding.binaryMessenger)
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    teardown()
+  }
+
+	var resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+    //if (result.resultCode == Activity.RESULT_OK) {
+      this.result!.success("sent") 
+      //this.result = null 
+      //return true 
+    //}
+	}
+
+	// override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent): Boolean { 
+	// 	this.result!.success("CHECK") 
+  //   if (requestCode == REQUEST_CODE_SEND_SMS && this.result != null) { 
+  //     this.result!.success("sent") 
+  //     this.result = null 
+  //     return true 
+  //   }
+  //   return false 
+  // }
+
+  private fun setupCallbackChannels(messenger: BinaryMessenger) {
+    mChannel = MethodChannel(messenger, "flutter_sms")
+    mChannel.setMethodCallHandler(this)
+  }
+
+  private fun teardown() {
+    mChannel.setMethodCallHandler(null)
+  }
+
+  // V1 embedding entry point. This is deprecated and will be removed in a future Flutter
+  // release but we leave it here in case someone's app does not utilize the V2 embedding yet.
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "flutter_sms")
-      channel.setMethodCallHandler(FlutterSmsPlugin(registrar))
+      val inst = FlutterSmsPlugin()
+      inst.activity = registrar.activity()
+      inst.setupCallbackChannels(registrar.messenger())
     }
   }
 
-  init {
-    this.activity = registrar.activity()
-		registrar.addActivityResultListener(this)
-  }
-
   override fun onMethodCall(call: MethodCall, result: Result) {
-    this.result = result
-    when {
-        call.method == "sendSMS" -> {
+    when (call.method) {
+        "sendSMS" -> {
           if (!canSendSMS()) {
             result.error(
                     "device_not_capable",
@@ -43,12 +102,13 @@ class FlutterSmsPlugin(registrar: Registrar) : MethodCallHandler {
                     "A device may be unable to send messages if it does not support messaging or if it is not currently configured to send messages. This only applies to the ability to send text messages via iMessage, SMS, and MMS.")
             return
           }
-          val message = call.argument<String?>("message")
-          val recipients = call.argument<String?>("recipients")
-          sendSMS(result, recipients, message!!)
-          result.success("SMS Sent!")
+          val message = call.argument<String?>("message") ?: ""
+          val recipients = call.argument<String?>("recipients") ?: ""
+          val sendDirect = call.argument<Boolean?>("sendDirect") ?: false
+          sendSMS(result, recipients, message!!, sendDirect)
         }
-        call.method == "canSendSMS" -> result.success(canSendSMS())
+        "canSendSMS" -> result.success(canSendSMS())
+	
         else -> result.notImplemented()
     }
   }
@@ -59,26 +119,45 @@ class FlutterSmsPlugin(registrar: Registrar) : MethodCallHandler {
       return false
     val intent = Intent(Intent.ACTION_SENDTO)
     intent.data = Uri.parse("smsto:")
-    val activityInfo = intent.resolveActivityInfo(activity!!.packageManager, intent.flags)
+    val activityInfo = intent.resolveActivityInfo(activity!!.packageManager, intent.flags.toInt())
     return !(activityInfo == null || !activityInfo.exported)
-
   }
 
-  private fun sendSMS(result: Result, phones: String?, message: String?) {
+  private fun sendSMS(result: Result, phones: String, message: String, sendDirect: Boolean) {
+    if (sendDirect) {
+      sendSMSDirect(result, phones, message);
+    }
+    else {
+      sendSMSDialog(result, phones, message);
+    }
+  }
+
+  private fun sendSMSDirect(result: Result, phones: String, message: String) {
+    // SmsManager is android.telephony
+    val sentIntent = PendingIntent.getBroadcast(activity, 0, Intent("SMS_SENT_ACTION"), PendingIntent.FLAG_IMMUTABLE)
+    val mSmsManager = SmsManager.getDefault()
+    val numbers = phones.split(";")
+
+    for (num in numbers) {
+      Log.d("Flutter SMS", "msg.length() : " + message.toByteArray().size)
+      if (message.toByteArray().size > 80) {
+        val partMessage = mSmsManager.divideMessage(message)
+        mSmsManager.sendMultipartTextMessage(num, null, partMessage, null, null)
+      } else {
+        mSmsManager.sendTextMessage(num, null, message, sentIntent, null)
+      }
+    }
+
+    result.success("SMS Sent!")
+  }
+
+  private fun sendSMSDialog(result: Result, phones: String, message: String) {
     val intent = Intent(Intent.ACTION_SENDTO)
     intent.data = Uri.parse("smsto:$phones")
     intent.putExtra("sms_body", message)
     intent.putExtra(Intent.EXTRA_TEXT, message)
-   	intent.putExtra(Intent.EXTRA_STREAM, attachment);
-    activity?.startActivityForResult(intent, REQUEST_CODE_SEND_SMS)
+		this.result = result
+		resultLauncher.launch(intent)
+    // activity?.startActivityForResult(intent, REQUEST_CODE_SEND_SMS)
   }
-
- override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent): Boolean {
-   if (requestCode == REQUEST_CODE_SEND_SMS && this.result != null) {
-     this.result!!.success("finished")
-     this.result = null
-     return true
-   }
-   return false
- }
 }
